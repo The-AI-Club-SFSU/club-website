a3f59a439f8209bf9c28ec16a23c310ecd2fd643
devalto <51037255+dev-alto@users.noreply.github.com>
2025-09-01 02:46:44 -0700

    feat: updated assets and colors

--- src/app/shared/Socials.tsx
+++ src/app/shared/Socials.tsx
@@ -1,5 +1,5 @@
-import { SiDiscord, SiYoutube, SiLinkedin, SiInstagram, SiGithub, SiLinktree, SiSlideshare } from 'react-icons/si'
-import { MdOutlineEmail } from 'react-icons/md'
+import { SiDiscord, SiYoutube, SiLinkedin, SiInstagram, SiGithub } from 'react-icons/si'
+import { BsMailboxFlag } from 'react-icons/bs'
 
 import Link from 'next/link'
 
@@ -10,7 +10,7 @@ export default function Socials() {
                 href='https://discord.com/'
                 target='_blank'
             >
-                <button className='flex group w-[160px] sm:w-[180px] sm:hover:w-[200px] justify-center bg-blue-400/10 bg-opacity-80 rounded-md gap-2 py-3 2 active:scale-95 hover:bg-blue-800 hover:outline-4 hover:text-white text-xl font-bold active:outline-2 hover:outline-double border-neutral-800 hover:outline-blue-600 active:outline-neutral-200 duration-200 active:duration-200'>
+                <button className='flex group w-[160px] sm:w-[180px] sm:hover:w-[200px] justify-center bg-blue-400/15 bg-opacity-80 rounded-md gap-2 py-3 active:scale-95 hover:bg-blue-800 hover:outline-4 hover:text-white text-xl font-bold active:outline-2 hover:outline-double border-neutral-800 hover:outline-blue-600 active:outline-neutral-200 duration-200 active:duration-200'>
                     <SiDiscord className='my-auto group-hover:text-white group-hover:text-2xl transition-all duration-300' />
                     Discord
                 </button>
@@ -20,7 +20,7 @@ export default function Socials() {
                 href='https://www.youtube.com/'
                 target='_blank'
             >
-                <button className='flex group w-[160px] sm:w-[180px] sm:hover:w-[200px] justify-center bg-red-500/10 bg-opacity-80 rounded-md gap-2 py-3 2 active:scale-95 hover:bg-red-900 hover:outline-4 hover:text-white text-xl font-bold active:outline-2 hover:outline-double border-neutral-800 hover:outline-red-700 active:outline-neutral-200 duration-200 active:duration-200'>
+                <button className='flex group w-[160px] sm:w-[180px] sm:hover:w-[220px] justify-center bg-red-500/15 bg-opacity-80 rounded-md gap-2 py-3 active:scale-95 hover:bg-red-700 hover:outline-4 hover:text-white text-xl font-bold active:outline-2 hover:outline-double border-neutral-800 hover:outline-red-700 active:outline-neutral-200 duration-200 active:duration-200'>
                     <SiYoutube className='my-auto group-hover:text-white group-hover:text-2xl transition-all duration-300' />
                     YouTube
                 </button>
@@ -30,7 +30,7 @@ export default function Socials() {
                 href='https://www.linkedin.com/'
                 target='_blank'
             >
-                <button className='flex group w-[160px] sm:w-[180px] sm:hover:w-[200px] justify-center bg-blue-600/10 bg-opacity-80 rounded-md gap-2 py-3 2 active:scale-95 hover:bg-blue-600 hover:outline-4 hover:text-white text-xl font-bold active:outline-2 hover:outline-double border-neutral-800 hover:outline-blue-400 active:outline-neutral-200 duration-200 active:duration-200'>
+                <button className='flex group w-[160px] sm:w-[180px] sm:hover:w-[220px] justify-center bg-blue-600/15 bg-opacity-80 rounded-md gap-2 py-3 active:scale-95 hover:bg-blue-600 hover:outline-4 hover:text-white text-xl font-bold active:outline-2 hover:outline-double border-neutral-800 hover:outline-blue-400 active:outline-neutral-200 duration-200 active:duration-200'>
                     <SiLinkedin className='my-auto group-hover:text-white group-hover:text-2xl transition-all duration-300' />
                     LinkedIn
                 </button>
@@ -40,7 +40,7 @@ export default function Socials() {
                 href='https://www.instagram.com/'
                 target='_blank'
             >
-                <button className='flex group w-[160px] sm:w-[180px] sm:hover:w-[200px] justify-center bg-purple-900/10 bg-opacity-80 rounded-md gap-2 py-3 2 active:scale-95 hover:bg-purple-600 hover:outline-4 hover:text-white text-xl font-bold active:outline-2 hover:outline-double border-neutral-800 hover:outline-purple-400 active:outline-neutral-200 duration-200 active:duration-200'>
+                <button className='flex group w-[160px] sm:w-[180px] sm:hover:w-[220px] justify-center bg-pink-900/15 bg-opacity-80 rounded-md gap-2 py-3 active:scale-95 hover:bg-pink-600 hover:outline-4 hover:text-white text-xl font-bold active:outline-2 hover:outline-double border-neutral-800 hover:outline-pink-400 active:outline-neutral-200 duration-200 active:duration-200'>
                     <SiInstagram className='my-auto group-hover:text-white group-hover:text-2xl transition-all duration-300' />
                     Instagram
                 </button>
@@ -50,15 +50,15 @@ export default function Socials() {
                 href='https://github.com/'
                 target='_blank'
             >
-                <button className='flex group w-[160px] sm:w-[180px] sm:hover:w-[200px] justify-center bg-yellow-800/10 bg-opacity-80 rounded-md gap-1 py-3 2 active:scale-95 hover:bg-yellow-700 hover:outline-4 hover:text-white text-xl font-bold active:outline-2 hover:outline-double border-neutral-800 hover:outline-yellow-700 active:outline-neutral-200 duration-200 active:duration-200'>
+                <button className='flex group w-[160px] sm:w-[180px] sm:hover:w-[220px] justify-center bg-yellow-800/15 bg-opacity-80 rounded-md gap-1 py-3 active:scale-95 hover:bg-yellow-700 hover:outline-4 hover:text-white text-xl font-bold active:outline-2 hover:outline-double border-neutral-800 hover:outline-yellow-700 active:outline-neutral-200 duration-200 active:duration-200'>
                     <SiGithub className='my-auto group-hover:text-white group-hover:text-2xl transition-all duration-300' />
                     GitHub
                 </button>
             </Link>
 
             <Link href='mailto:a@gmail.com' target='_blank'>
-                <button className='flex group w-[160px] sm:w-[180px] sm:hover:w-[200px] justify-center bg-neutral-600 dark:bg-green-900/10 bg-opacity-80 rounded-md gap-2 py-3 2 active:scale-95 hover:bg-green-900 hover:outline-4 hover:text-white text-xl font-bold active:outline-2 hover:outline-double border-neutral-800 hover:outline-green-700 active:outline-neutral-200 duration-200 active:duration-200'>
-                    <MdOutlineEmail className='my-auto group-hover:text-white group-hover:text-2xl transition-all duration-300' />
+                <button className='flex group w-[160px] sm:w-[180px] sm:hover:w-[220px] justify-center bg-neutral-600 dark:bg-lime-900/15 bg-opacity-80 rounded-md gap-2 py-3 2 active:scale-95 hover:bg-lime-700 hover:outline-4 hover:text-white text-xl font-bold active:outline-2 hover:outline-double border-neutral-800 hover:outline-lime-700 active:outline-neutral-200 duration-200 active:duration-200'>
+                    <BsMailboxFlag className='my-auto group-hover:text-white group-hover:text-2xl transition-all duration-300' />
                     Email
                 </button>
             </Link>
