3c555ae62206cff931cc87cd8307e88df2309b12
devalto <51037255+dev-alto@users.noreply.github.com>
2025-09-07 17:02:24 -0700

    feat: changed navbar logo animation when hovering

--- src/app/shared/Navbar.tsx
+++ src/app/shared/Navbar.tsx
@@ -93,7 +93,7 @@ export function Navbar(): React.ReactNode {
                 animate={{ opacity: 1 }}
                 transition={{ duration: 1, delay: 1, ease: 'easeOut' }}
             >
-                <Link href='/' className='z-10 absolute left-0 flex items-center justify-center gap-x-2 drop-shadow-lg ml-4 hover:rotate-180 active:animate-pulse duration-500'>
+                <Link href='/' className='z-10 absolute left-0 flex items-center justify-center gap-x-2 drop-shadow-lg ml-4 hover:animate-pulse duration-500'>
                     <Image
                         width={48}
                         height={48}
